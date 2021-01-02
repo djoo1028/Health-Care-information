33159fbf5a0f0718d950b843bf215c8bf4675f68
Roy Joo <52684315+djoo1028@users.noreply.github.com>
2021-01-02 13:26:32 -0800

    Add files via upload

---
+++ project/README.md
@@ -0,0 +1,4 @@
+# HuskyDataIncWebApp
+Husky Data Inc Web application with Database Integration
+
+Website is connected with MySQL and requires config.php. 
---
+++ project/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DBHOST', 'localhost');
+    define('DBNAME', 'project3db');
+    define('DBUSER', 'user');
+    define('DBPASS', 'password');
+    define('DBCONNSTRING','mysql:dbname=project3db;charset=utf8mb4');
+?>
